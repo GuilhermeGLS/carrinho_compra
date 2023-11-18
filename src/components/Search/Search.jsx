3081b1e40cd600cdb127306394715ccb148d95b5
Guilherme Lima e Silva <guilhermelimaesilva.p@gmail.com>
2023-11-18 09:22:33 -0300

    add: event de busca no input

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,14 +1,26 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './Search.css'
 
 function Search() {
-  // const [count, setCount] = useState(0)
+  const [search, setSearch] = useState("Telefone")
+  
+  const buscar = (e) => {
+    e.preventDefault()
+    alert(search)
+  }
 
   return (
     <>
       {/* NAV */}
-          <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+          <form className="d-flex" role="search" onSubmit={buscar}>
+            <input 
+            className="form-control me-2" 
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            />
               <button className="btn btn-outline-success" type="submit">Search</button>
           </form>
         
